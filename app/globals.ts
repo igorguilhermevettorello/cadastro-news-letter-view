00e5e5898d80f34ac0f894a24f4d1c2c64ca8bf6
Igor Vettorello <igor.vettorello@hotmail.com>
2018-04-04 20:08:30 -0300

    setar web service

--- app/globals.ts
+++ app/globals.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 Injectable()
 
 export class Globals {
-    public url : string = 'http://localhost:3100';
-    //public url : string = 'https://cadastro-habitacional-server.herokuapp.com';
+    //public url : string = 'http://localhost:3100';
+    public url : string = 'https://cadastro-habitacional-server.herokuapp.com';
 }
