dbc7e2a6532bdea1235387c616ba4605df23b195
Igor Vettorello <igor.vettorello@hotmail.com>
2018-04-04 20:16:56 -0300

    setar web service

--- app/globals.js
+++ app/globals.js
@@ -3,7 +3,8 @@ var core_1 = require('@angular/core');
 core_1.Injectable();
 var Globals = (function () {
     function Globals() {
-        this.url = 'http://localhost:3100';
+        //public url : string = 'http://localhost:3100';
+        this.url = 'https://cadastro-habitacional-server.herokuapp.com';
     }
     return Globals;
 }());
